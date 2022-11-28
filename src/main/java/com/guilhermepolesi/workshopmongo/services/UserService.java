e0d5ca9671d026bf53ce2f8ce4bdc37d85a64f9f
guilhermepolesi <guilherme-polesi@hotmail.com>
2022-11-28 18:42:45 -0300

    Insert user with POST

--- src/main/java/com/guilhermepolesi/workshopmongo/services/UserService.java
+++ src/main/java/com/guilhermepolesi/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.guilhermepolesi.workshopmongo.domain.User;
+import com.guilhermepolesi.workshopmongo.dto.UserDTO;
 import com.guilhermepolesi.workshopmongo.repository.UserRepository;
 import com.guilhermepolesi.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
