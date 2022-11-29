a2fec6b12820810bb780b68ba858411ee4bade92
guilhermepolesi <guilherme-polesi@hotmail.com>
2022-11-29 17:04:50 -0300

    Endpoint to return a user's posts

--- src/main/java/com/guilhermepolesi/workshopmongo/resources/UserResources.java
+++ src/main/java/com/guilhermepolesi/workshopmongo/resources/UserResources.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.guilhermepolesi.workshopmongo.domain.Post;
 import com.guilhermepolesi.workshopmongo.domain.User;
 import com.guilhermepolesi.workshopmongo.dto.UserDTO;
 import com.guilhermepolesi.workshopmongo.services.UserService;
@@ -57,6 +58,12 @@ public class UserResources {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 
 }
